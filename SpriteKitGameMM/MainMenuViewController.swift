5b4475d209392a8b5bfee56870698b6ef8b4ce75
Aaron Bradley <aaronb@Aarons-MacBook-Air.local>
2015-02-22 11:43:45 -0600

    initial storyboard setup for soldier selection

---
+++ SpriteKitGameMM/MainMenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainMenuViewController.swift
+//  SpriteKitGameMM
+//
+//  Created by Aaron Bradley on 2/22/15.
+//  Copyright (c) 2015 Aaron Bradley. All rights reserved.
+//
+
+import Foundation
+
+class MainMenuViewController: UIViewController {
+
+
+    @IBOutlet var imageViewSoldier: UIImageView!
+
+
+
+    @IBAction func buttonTapChangeSoldier(sender: UIButton) {
+    }
+
+}
